bfabbda61970c83abaed42622feba06448aee390
Churreesha Harden <czhverdant@gmail.com>
2022-11-10 03:40:43 -0500

    Update app.js

--- fend-refresh-2019/projects/landing-page/js/app.js
+++ fend-refresh-2019/projects/landing-page/js/app.js
@@ -42,45 +42,99 @@ const sections = document.querySelectorAll("section");
 
 // build the nav
 
-
-function navigation(){
-    for (let i = 0; i <= sections.length + 1;) {
-        const section = sections[i++];
-        const li = document.createElement("sectionList");
-
-        li.innerHTML =`<li><a class="menu__link" href="#${section && section.id}">${section.dataset.nav}</a></li>`;
-
-        navBarList.prepend(li);
-   }
+//looping through sections
+function navigation() {
+  for (let i = 0; i < sections.length; + 1) {
+    let section = sections[i++];
+    li = document.createElement("sectionList");
+    li.innerHTML = `<li><a class="menu__link" href="#${section?.id}">${section && section.dataset.nav}</a></li>`;
+    navBarList.prepend(li);
+  }
 };
+
 navigation();
-
-
-
-
-
-
-
-
 
 
 // Add class 'active' to section when near top of viewport
 
+// Removing active sections
+let activeRemove = (section) => {
+  if (section) {
+    section.classList.remove('your-active-class');
+    //section.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
+    section.style.cssText = "background-color: rgb(136,203,171);";
+  } else {
+    section.classList.add('your-active-class');
+    section.style.cssText = "background-color: pink;";
+  };
+};
+// Set sections as active
+let activeAdd = (visibleViewport, section) => {
+  if (visibleViewport) {
+    section.classList.add('your-active-class');
+    section.style.cssText = "background-color: pink;";
+  } else {
+    section.classList.remove('your-active-class');
+    section.style.cssText = "background-color: rgb(136,203,171);";
+  };
 
+};
+
+//making the section active
+
+const activateSection = () => {
+
+  for (let i = 0; i < sections.length; + 1) {
+    let section = sections[i++];
+
+    visibleViewport = () => section.getBoundingClientRect().top < 285 && section.getBoundingClientRect().top >= -285;
+
+    activeRemove(section);
+    activeAdd(visibleViewport(), section);
+
+
+  };
+
+};
+
+window.addEventListener('scroll', activateSection);
 
 // Scroll to anchor ID using scrollTO event
 
 
-/**
- * End Main Functions
- * Begin Events
- *
-*/
+function smooth() {
+  const sectionScroll = document.querySelectorAll(".menu__link")
+  for (let Scroller of sectionScroll) {
+    Scroller.addEventListener('click', function (event) {
 
-// Build menu
 
-// Scroll to section on link click
+      event.preventDefault();
+      //implementing scrollIntoView providing a smooth effect
+      //when you click on each section in the nav
+      const secId = (this.getAttribute('href'));
+      document.querySelector(secId).scrollIntoView({
+        // Scroll to section on link click
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest"
+      });
 
-// Set sections as active
+
+    });
+
+  };
+};
+smooth();
+
+
+
+
+ 
+
+
+
+
+
+
 
 
